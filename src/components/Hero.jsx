2ec2a8a441cb5294e0d410b04b10fdd396787e16
elsieyli <elsie.snowflake1@gmail.com>
2024-08-15 21:47:29 -0400

    Finish navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {motion} from'framer-motion';
+import {styles} from '../styles';
+import {ComputerCanvas} from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} sbsolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+         <div className="flex flex-col">
+
+         </div>
+      </div>
+    </section>
   )
 }
 
