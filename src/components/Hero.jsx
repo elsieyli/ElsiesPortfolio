b4223002a110c5dfda7d83fb990f499c83fe2b9c
elsieyli <elsie.snowflake1@gmail.com>
2024-08-15 22:57:48 -0400

    adding text and formatting page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import {motion} from'framer-motion';
 import {styles} from '../styles';
-import {ComputerCanvas} from './canvas';
+//import {ComputerCanvas} from './canvas';
 
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} sbsolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
-         <div className="flex flex-col">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+         <div className="flex flex-col justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
+            <div className="w-1 sm:h-80 h-40 violet-gradient"/>
+         </div>
+         <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Elsie</span></h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I'm a versatile software developer in full-stack development, mastering multiple languages, frameworks, and agile methodologies.
 
+            </p>
          </div>
       </div>
     </section>
