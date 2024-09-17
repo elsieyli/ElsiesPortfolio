7cb4158d486b7fa8312a84436c1971c7c4746bd4
elsieyli <elsie.snowflake1@gmail.com>
2024-09-16 20:55:57 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,22 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div>
-      Under Maintenance...🚧
-    </div>
-  </StrictMode>,
-)
+   <StrictMode>
+     <div
+       style={{
+         display: 'flex',
+         justifyContent: 'center',
+         alignItems: 'center',
+         height: '100vh',
+         fontSize: '32px',
+         fontWeight: 'bold',
+         textAlign: 'center',
+       }}
+     >
+       Under Maintenance...🚧
+     </div>
+   </StrictMode>,
+ );
+
+ //<App/> WAS HERE 
+ 
